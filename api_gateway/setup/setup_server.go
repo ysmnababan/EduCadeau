cae2a61d557d1ee67e103cd72c86f5a51a97f94f
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-07-03 01:43:36 +0700

    feat: add apm elastic middleware

--- api_gateway/setup/setup_server.go
+++ api_gateway/setup/setup_server.go
@@ -5,9 +5,11 @@ import (
 	"api_gateway/helper"
 	"api_gateway/pb"
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.elastic.co/apm/module/apmechov4/v2"
 )
 
 type Handler struct {
@@ -16,6 +18,8 @@ type Handler struct {
 
 func SetupRESTServer(e *echo.Echo, h *Handler) {
 	e.Use(middleware.Recover())
+	e.Use(apmechov4.Middleware())
+
 	// using logger for each api
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
@@ -24,8 +28,15 @@ func SetupRESTServer(e *echo.Echo, h *Handler) {
 			fmt.Println("            EDU CADEU")
 			fmt.Println("==================================")
 
-			helper.Logging(c).Info("Calling an API")
-			return next(c)
+			// Wrap the response writer to capture the status code
+			rr := &helper.ResponseRecorder{ResponseWriter: c.Response().Writer, Status: http.StatusOK}
+			c.Response().Writer = rr
+
+			err := next(c)
+
+			// Log the request details including the status code
+			helper.Logging(c).Info("request detail")
+			return err
 		}
 	})
 
