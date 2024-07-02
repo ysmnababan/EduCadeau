abd88d345a5f31aafb3787b846c78f7042a03d78
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-07-02 22:57:30 +0700

    feat: add error parser from grpc server

--- api_gateway/helper/eh.go
+++ api_gateway/helper/eh.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -75,53 +76,82 @@ func ParseError(err error, ctx echo.Context) error {
 	return ctx.JSON(status, map[string]interface{}{"message": message})
 }
 
-func ParseErrorGRPC(err error) error {
-	Logging(nil).Error("ERR: ", err)
-	code := codes.OK
+// func ParseErrorGRPC(err error) error {
+// 	Logging(nil).Error("ERR: ", err)
+// 	code := codes.OK
+// 	message := ""
+// 	switch {
+// 	case errors.Is(err, ErrNoUser):
+// 		code = codes.NotFound
+// 		message = "No User found"
+// 	case errors.Is(err, ErrNoData):
+// 		code = codes.NotFound
+// 		message = "No data found"
+// 	case errors.Is(err, ErrUnsufficientBalance):
+// 		code = codes.InvalidArgument
+// 		message = "unsufficient balance"
+// 	case errors.Is(err, ErrParam):
+// 		code = codes.InvalidArgument
+// 		message = "error or missing param"
+// 	case errors.Is(err, ErrBindJSON):
+// 		code = codes.InvalidArgument
+// 		message = "Bad request"
+// 	case errors.Is(err, ErrInvalidId):
+// 		code = codes.InvalidArgument
+// 		message = "Invalid ID"
+// 	case errors.Is(err, ErrCredential):
+// 		code = codes.InvalidArgument
+// 		message = "email or password missmatch"
+// 	case errors.Is(err, ErrUserExists):
+// 		code = codes.AlreadyExists
+// 		message = "User Already Exists"
+// 	case errors.Is(err, ErrMustAdmin):
+// 		code = codes.PermissionDenied
+// 		message = "Admin privilege only"
+// 	case errors.Is(err, ErrOnlyUser):
+// 		code = codes.PermissionDenied
+// 		message = "User privilege only"
+// 	case errors.Is(err, ErrNoUpdate):
+// 		code = codes.AlreadyExists
+// 		message = "Data is the same"
+// 	case errors.Is(err, ErrQuery):
+// 		fallthrough
+// 	case errors.Is(err, ErrGeneratedPwd):
+// 		fallthrough
+// 	default:
+// 		code = codes.Internal
+// 		message = "Unknown error:" + err.Error()
+// 	}
+
+// 	// log.Println(map[string]interface{}{"message": message, "status": status})
+// 	return status.Errorf(code, message)
+// }
+
+func ParseErrorGRPC(err error, ctx echo.Context) error {
+	stat := http.StatusOK
 	message := ""
-	switch {
-	case errors.Is(err, ErrNoUser):
-		code = codes.NotFound
-		message = "No User found"
-	case errors.Is(err, ErrNoData):
-		code = codes.NotFound
-		message = "No data found"
-	case errors.Is(err, ErrUnsufficientBalance):
-		code = codes.InvalidArgument
-		message = "unsufficient balance"
-	case errors.Is(err, ErrParam):
-		code = codes.InvalidArgument
-		message = "error or missing param"
-	case errors.Is(err, ErrBindJSON):
-		code = codes.InvalidArgument
-		message = "Bad request"
-	case errors.Is(err, ErrInvalidId):
-		code = codes.InvalidArgument
-		message = "Invalid ID"
-	case errors.Is(err, ErrCredential):
-		code = codes.InvalidArgument
-		message = "email or password missmatch"
-	case errors.Is(err, ErrUserExists):
-		code = codes.AlreadyExists
-		message = "User Already Exists"
-	case errors.Is(err, ErrMustAdmin):
-		code = codes.PermissionDenied
-		message = "Admin privilege only"
-	case errors.Is(err, ErrOnlyUser):
-		code = codes.PermissionDenied
-		message = "User privilege only"
-	case errors.Is(err, ErrNoUpdate):
-		code = codes.AlreadyExists
-		message = "Data is the same"
-	case errors.Is(err, ErrQuery):
-		fallthrough
-	case errors.Is(err, ErrGeneratedPwd):
-		fallthrough
-	default:
-		code = codes.Internal
-		message = "Unknown error:" + err.Error()
+	if st, ok := status.FromError(err); ok {
+		message = st.Message()
+
+		switch st.Code() {
+		case codes.NotFound:
+			stat = http.StatusNotFound
+		case codes.InvalidArgument:
+			stat = http.StatusBadRequest
+		case codes.AlreadyExists:
+			stat = http.StatusBadRequest
+		case codes.PermissionDenied:
+			stat = http.StatusUnauthorized
+		case codes.Internal:
+			fallthrough
+		default:
+			stat = http.StatusInternalServerError
+			message = "Unknown error:" + err.Error()
+		}
+	} else {
+		fmt.Printf("not able to parse error returned %v", err)
+		message = "Internal error parsing grpc"
 	}
 
-	// log.Println(map[string]interface{}{"message": message, "status": status})
-	return status.Errorf(code, message)
+	return ctx.JSON(stat, map[string]interface{}{"message": message})
 }
