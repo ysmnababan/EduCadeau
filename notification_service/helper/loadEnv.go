e5e9de7697f6d5e7ff53c8d0070a566839e97bc3
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-07-05 17:01:57 +0700

    feat: add deployment for notif_service

--- notification_service/helper/loadEnv.go
+++ notification_service/helper/loadEnv.go
@@ -10,6 +10,7 @@ var (
 	RABBIT_MQ_ADDR        string
 	USER_REGISTER_CHANNEL string
 	USER_EDIT_CHANNEL     string
+	PORT                  string
 )
 
 func LoadEnv() {
@@ -21,4 +22,6 @@ func LoadEnv() {
 	RABBIT_MQ_ADDR = os.Getenv("RABBIT_MQ_ADDR")
 	USER_REGISTER_CHANNEL = os.Getenv("USER_REGISTER_CHANNEL")
 	USER_EDIT_CHANNEL = os.Getenv("USER_EDIT_CHANNEL")
+	PORT = os.Getenv("PORT")
+
 }
--- notification_service/main.go
+++ notification_service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"notification_service/handler"
 	helper "notification_service/helper"
 )
@@ -9,11 +10,22 @@ import (
 func main() {
 	// load the .env
 	helper.LoadEnv()
-
 	//setup the receiver
 	forever := make(chan bool)
 	go handler.NotifyUserRegister()
 	go handler.NotifyUserEditData()
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	go func() {
+		log.Println("Starting HTTP server on port 8080")
+		if err := http.ListenAndServe(":"+helper.PORT, nil); err != nil {
+			log.Fatalf("Error starting HTTP server: %v", err)
+		}
+	}()
+
 	log.Println("NOTIFICATION SERVICE STARTED...")
 	<-forever
 }
