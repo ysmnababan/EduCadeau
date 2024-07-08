72a6a7fbc094df724ceba64cfcf95eb93349af70
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-07-08 16:14:35 +0700

    fix: differentiate product and service of donation

--- donation_service/models/donation_details_model.go
+++ donation_service/models/donation_details_model.go
@@ -28,16 +28,16 @@ type CreateDonationReq struct {
 }
 
 type EditDonationReq struct {
-	DonationID    primitive.ObjectID `json:"donation_id,omitempty" bson:"donation_id,omitempty"`
-	RecipientID   uint               `json:"recipient_id,omitempty" bson:"recipient_id,omitempty"`
-	DonationName  string             `json:"donation_name" bson:"donation_name"`
-	TargetAmount  float64            `json:"target_amount" bson:"target_amount"`
-	Description   string             `json:"description,omitempty" bson:"description,omitempty"`
-	DonationType  string             `json:"donation_type,omitempty" bson:"donation_type,omitempty"`
-	Tag           string             `json:"tag,omitempty" bson:"tag,omitempty"`
-	SenderAddress string             `json:"sender_address,omitempty" bson:"sender_address,omitempty"`
-	RelatedLink   string             `json:"related_link,omitempty" bson:"related_link,omitempty"`
-	Notes         string             `json:"notes,omitempty" bson:"notes,omitempty"`
+	DonationID        primitive.ObjectID `json:"donation_id,omitempty" bson:"donation_id,omitempty"`
+	RecipientID       uint               `json:"recipient_id,omitempty" bson:"recipient_id,omitempty"`
+	DonationName      string             `json:"donation_name" bson:"donation_name"`
+	TargetAmount      float64            `json:"target_amount" bson:"target_amount"`
+	Description       string             `json:"description,omitempty" bson:"description,omitempty"`
+	MiscellaneousCost float64            `json:"miscellaneous_cost,omitempty" bson:"miscellaneous_cost,omitempty"`
+	Tag               string             `json:"tag,omitempty" bson:"tag,omitempty"`
+	SenderAddress     string             `json:"sender_address,omitempty" bson:"sender_address,omitempty"`
+	RelatedLink       string             `json:"related_link,omitempty" bson:"related_link,omitempty"`
+	Notes             string             `json:"notes,omitempty" bson:"notes,omitempty"`
 }
 
 type DonationDetailResp struct {
--- donation_service/models/donation_model.go
+++ donation_service/models/donation_model.go
@@ -5,10 +5,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Donation struct {
 	ID                primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	RecipientID       uint               `json:"recipient_id,omitempty" bson:"recipient_id,omitempty"`
-	DonationName      string             `json:"donation_name" bson:"donation_name"`
-	CreatedAt         string             `json:"created_at" bson:"created_at"`
-	Status            string             `json:"status" bson:"status"`
-	TargetAmount      float64            `json:"target_amount" bson:"target_amount"`
-	AmountCollected   float64            `json:"amount_collected" bson:"amount_collected"`
+	DonationName      string             `json:"donation_name,omitempty" bson:"donation_name,omitempty"`
+	CreatedAt         string             `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	Status            string             `json:"status,omitempty" bson:"status,omitempty"`
+	TargetAmount      float64            `json:"target_amount,omitempty" bson:"target_amount,omitempty"`
+	AmountCollected   float64            `json:"amount_collected,omitempty" bson:"amount_collected,omitempty"`
 	MiscellaneousCost float64            `json:"miscellaneous_cost,omitempty" bson:"miscellaneous_cost,omitempty"`
 }
