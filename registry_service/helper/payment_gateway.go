c09d7571ee2942c93bd58cb0f561c92ac8b371a3
aFemzz <andhika.mailss@gmail.com>
2024-07-10 14:53:39 +0700

    feat:helper xendit

--- registry_service/helper/payment_gateway.go
+++ registry_service/helper/payment_gateway.go
@@ -1,7 +1,65 @@
 package helper
 
-import "registry_service/pb/pbDonationRegistry"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"registry_service/models"
+	"registry_service/pb/pbDonationRegistry"
+)
+
+const (
+	XenditURL = "https://api.xendit.co/v2/invoices"
+)
 
 func PaymentGateway(amount float64, detail *pbDonationRegistry.DonationResp) {
-	// write your code here
+	XenditAPIKey := os.Getenv("XENDIT_SECRET_KEY")
+	// Prepare the request payload
+	requestPayload := models.XenditInvoiceRequest{
+		ExternalID:  fmt.Sprintf("donation_%d", detail.RecipientId),
+		Amount:      amount,
+		Description: detail.Description,
+	}
+
+	payloadBytes, err := json.Marshal(requestPayload)
+	if err != nil {
+		log.Fatalf("Error marshalling request payload: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", XenditURL, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		log.Fatalf("Error creating HTTP request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(XenditAPIKey, "")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error sending HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body for detailed error message
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Error response from Xendit: %v\nResponse body: %s", resp.Status, string(body))
+	}
+
+	var invoiceResponse models.XenditInvoiceResponse
+	err = json.Unmarshal(body, &invoiceResponse)
+	if err != nil {
+		log.Fatalf("Error decoding response body: %v", err)
+	}
+	fmt.Println(invoiceResponse.ID)
+	fmt.Printf("Invoice created: %s\n", invoiceResponse.InvoiceURL)
 }
--- registry_service/models/payment_model.go
+++ registry_service/models/payment_model.go
@@ -10,10 +10,21 @@ type Payment struct {
 	PaymentAmount float64            `json:"payment_amount" bson:"payment_amount"`
 }
 
-type PaymentReq struct{
+type PaymentReq struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	RegistryID    primitive.ObjectID `json:"registry_id" bson:"registry_id"`
 	PaymentDate   string             `json:"payment_date" bson:"payment_date"`
 	PaymentMethod string             `json:"payment_method" bson:"payment_method"`
 	PaymentAmount float64            `json:"payment_amount" bson:"payment_amount"`
+}
+
+type XenditInvoiceRequest struct {
+	ExternalID  string  `json:"external_id"`
+	Amount      float64 `json:"amount"`
+	Description string  `json:"description"`
+}
+
+type XenditInvoiceResponse struct {
+	ID         string `json:"id"`
+	InvoiceURL string `json:"invoice_url"`
 }
